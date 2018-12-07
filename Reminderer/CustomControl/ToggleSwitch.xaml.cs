dde5852eb437bfa372cd17d6ed8e6706fb592e43
Youngbin Kim <lyakyb@gmail.com>
2018-12-07 21:42:19 +0900

    updated dimension calculation

--- Reminderer/CustomControl/ToggleSwitch.xaml.cs
+++ Reminderer/CustomControl/ToggleSwitch.xaml.cs
@@ -50,40 +50,52 @@ namespace Reminderer.CustomControl
 
         private void calculateDimensions()
         {
-            innerCircle.Width = background.ActualWidth * 0.97;
+            innerCircle.Width = background.ActualWidth;
             innerCircle.Height = background.ActualHeight * 0.8;
-            innerCircle.RadiusX = innerCircle.ActualWidth * 0.4;
+            innerCircle.RadiusX = innerCircle.ActualWidth * 0.1;
             innerCircle.RadiusY = innerCircle.ActualHeight * 1;
-            
-            Console.WriteLine($"bgWidth: {background.ActualWidth.ToString()} , bgHeight: {background.ActualHeight.ToString()}");
 
-            toggleEllipse.Width = innerCircle.Height * 0.95;
-            toggleEllipse.Height = innerCircle.Height * 0.95;
-            Thickness newMargin = new Thickness(innerCircle.Width / 2 - toggleEllipse.Width / 2, innerCircle.ActualHeight - toggleEllipse.Height, innerCircle.Width / 2 - toggleEllipse.Width / 2, innerCircle.ActualHeight - toggleEllipse.Height);
-           
-            newMargin.Left = newMargin.Left - toggleEllipse.Width * 0.5;
-            newMargin.Right = newMargin.Right + toggleEllipse.Width * 0.5;
-            toggleEllipse.Margin = _offPosMargin = newMargin;
+            toggleEllipse.Width = innerCircle.Height * 0.9;
+            toggleEllipse.Height = innerCircle.Height * 0.9;
 
-            _onPosMargin = newMargin;
-            _onPosMargin.Left = _onPosMargin.Left + toggleEllipse.Width * 0.5 * 2;
-            _onPosMargin.Right = _onPosMargin.Right - toggleEllipse.Width * 0.5 * 2;
-            _deltaX = toggleEllipse.Width * 0.5;
+            double desiredGap = innerCircle.Height * 0.1;
+
+            Thickness currentMargin = innerCircle.Margin;
+            double offMarginLeft = desiredGap;
+            double offMarginRight = innerCircle.Width - toggleEllipse.Width - desiredGap;
+            double onMarginLeft = innerCircle.Width - desiredGap;
+            double onMarginRight = desiredGap;
+
+            _offPosMargin = new Thickness(offMarginLeft, desiredGap, offMarginRight, desiredGap);
+            _onPosMargin = new Thickness(onMarginLeft, desiredGap, onMarginRight, desiredGap);
+
+            toggleEllipse.Margin = _offPosMargin;
+
+            _deltaX = onMarginLeft - onMarginRight - toggleEllipse.Width;
+
+            Console.WriteLine($"innerCircle width: {innerCircle.Width} and height: {innerCircle.Height}");
+            Console.WriteLine($"ellipse width: {toggleEllipse.Width}");
+            Console.WriteLine($"currentMargin: {toggleEllipse.Margin}");
+            Console.WriteLine($"desiredGap: {desiredGap}");
+            Console.WriteLine($"deltaX: {_deltaX}");
+
+
         }
-        
+
         private void clicked(object sender, RoutedEventArgs e)
         {
             if (_toggled)
             {
                 DoubleAnimation db = new DoubleAnimation();
-                db.From = _deltaX * 2;
-                db.To = innerCircle.Margin.Left;
+                db.From = _deltaX;
+                db.To = 0;
                 db.Duration = new Duration(TimeSpan.FromSeconds(0.5));
                 TranslateTransform tt = new TranslateTransform();
                 toggleEllipse.RenderTransform = tt;
 
                 tt.BeginAnimation(TranslateTransform.XProperty, db);
-               
+
+                Console.WriteLine($"TO: {db.To}");
                 ColorAnimation color = new ColorAnimation();
                 color.From = (Color)ColorConverter.ConvertFromString(DEFAULT_ON_COLOR_HEX_VALUE);
                 color.To = (Color)ColorConverter.ConvertFromString(DEFAULT_OFF_COLOR_HEX_VALUE);
@@ -95,8 +107,8 @@ namespace Reminderer.CustomControl
             } else
             {
                 DoubleAnimation db = new DoubleAnimation();
-                db.From = innerCircle.Margin.Left;
-                db.To = _deltaX * 2;
+                db.From = 0;
+                db.To = _deltaX;
                 db.Duration = new Duration(TimeSpan.FromSeconds(0.5));
                 TranslateTransform tt = new TranslateTransform();
                 toggleEllipse.RenderTransform = tt;
